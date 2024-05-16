da14ceb1b9b8c5658931bd05617213cbd7e511dc
cicer <git.use@outlook.com>
2024-05-16 06:04:14 -0500

    init

---
+++ src/main/java/com/cico/backubicatp/BackUbicatpApplication.java
@@ -0,0 +1,13 @@
+package com.cico.backubicatp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackUbicatpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackUbicatpApplication.class, args);
+	}
+
+}
